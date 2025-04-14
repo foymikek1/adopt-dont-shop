f339059997fb4e8ad3e65a718e8f5be6fce7c222
foymikek <foymikek@gmail.com>
2025-04-14 16:48:10 -0600

    Add application model test refactor. Known shoulda-matchers bug, wrote test in rspec. All tests passing.

--- spec/models/application_spec.rb
+++ spec/models/application_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Application, type: :model do
   end
 
   describe "validations" do
+    application = Application.create!(name: "Bob", address: "300 Power St", city: "Erie", state: "CO", zip_code: 91638, description: "Good fur-parent.", status: 0)
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:address) }
     it { should validate_presence_of(:city) }
@@ -14,6 +15,6 @@ RSpec.describe Application, type: :model do
     it { should validate_numericality_of(:zip_code) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:status) }
-    it { should validate_numericality_of(:status) }
+    it { expect(application.status).to eq("In_progress") }
   end
 end
